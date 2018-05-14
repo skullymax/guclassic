8535a5acac8db09def9ee0dd6c667f5f073435d5
skullymax <38359146+skullymax@users.noreply.github.com>
2018-05-14 22:46:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ client.on("message", async message => {
     message.channel.send("**Generating...**");
       message.author.send(doMagic8BallVoodoo());
       message.author.send("Join to official discord https://discord.gg/VAwQKQG");
+      message.author.send("Check Tutorials from EyzAlts Owner https://goo.gl/C49P28 or https://goo.gl/7eFw7j");
   }
 
   if(command === "clear") {
